c753b599f3fd940defab519e3a3b835381850d56
mohit7feb <45337317+mohit7feb@users.noreply.github.com>
2024-08-07 20:39:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -65,6 +65,15 @@ $traffic_Shield_Tre9854->run();
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-3YE1F5FTY0"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-3YE1F5FTY0');
+</script>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>V Gardener</title>
