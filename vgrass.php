f493bfe0d22c8de79932bed768230bd1aaeb2bb0
mohit7feb <45337317+mohit7feb@users.noreply.github.com>
2024-08-21 19:33:13 +0530

    Add files via upload

---
+++ vgrass.php
@@ -0,0 +1,336 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Natural Grass</title>
+<meta name="description" content="">
+<meta name="author" content="">
+
+<!-- Favicons
+    ================================================== -->
+<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+
+<!-- Bootstrap -->
+<link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.css">
+
+<!-- Slider
+    ================================================== -->
+<link href="css/owl.carousel.css" rel="stylesheet" media="screen">
+<link href="css/owl.theme.css" rel="stylesheet" media="screen">
+
+<!-- Stylesheet
+    ================================================== -->
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<!-- <link rel="stylesheet" type="text/css" href="css/style1.css"> -->
+<link rel="stylesheet" type="text/css" href="css/nivo-lightbox/nivo-lightbox.css">
+<link rel="stylesheet" type="text/css" href="css/nivo-lightbox/default.css">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+
+
+<style>
+    .portfolio-item.d-flex {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.portfolio-item .hover-bg {
+  flex-basis: 50%;
+}
+
+.portfolio-item .content {
+  flex-basis: 50%;
+  padding-left: 20px;
+}
+
+@media (max-width: 768px) {
+  .portfolio-item.d-flex {
+    flex-direction: column;
+  }
+
+  .portfolio-item .hover-bg,
+  .portfolio-item .content {
+    flex-basis: 100%;
+    padding-left: 0;
+  }
+
+  .portfolio-item .hover-bg img {
+    width: 100%;
+    height: auto;
+  }
+}
+
+</style>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+<!-- Navigation
+    ==========================================-->
+<nav id="menu" class="navbar navbar-default navbar-fixed-top">
+  <div class="container"> 
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+      <a class="navbar-brand page-scroll" href="#page-top">V Gardener</a> </div>
+    
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="index.php" class="page-scroll">About</a></li>
+        <li><a href="index.php" class="page-scroll">Services</a></li>
+        <li><a href="index.php" class="page-scroll">Gallery</a></li>
+        <li><a href="index.php" class="page-scroll">Testimonials</a></li>
+        <li><a href="ngrass.php" class="page-scroll">Natural Grass</a></li>
+        <li><a href="vgrass.php" class="page-scroll">Vetiver Grass</a></li>
+        <li><a href="policy.php" class="page-scroll">Privacy Policy</a></li>
+        <li><a href="#contact" class="page-scroll">Contact</a></li>
+      </ul>
+    </div>
+    <!-- /.navbar-collapse --> 
+  </div>
+  <!-- /.container-fluid --> 
+</nav>
+<!-- Header -->
+<header id="header">
+  <div class="intro">
+    <div class="overlay">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2 intro-text">
+            <h1>Vetiver Grass</h1>
+            <p>We are offering Natural Vetiver Grass.<br>
+            Provides you with the best range.</p>
+            <!-- <a href="tel:+18557616800" class="btn btn-custom btn-lg page-scroll" style="text-transform:none; border-radius:10px;">Know More</a> -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+
+<!-- Tips Section -->
+<div id="about">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-md-6">
+        <div class="about-text">
+          <h2> Natural <span>Vetiver Grass</span></h2>
+          <hr>
+          <p>The central component of The Vetiver System is Vetiver Grass (Chrysopogon zizanioides), a sterile, non-invasive tropical bunchgrass. It is used throughout the tropics in a wide-range of applications including erosion control, degraded land remediation, soil conservation and runoff retention, remediation of polluted land and water bodies, in constructed wetland sanitation systems, for supplemental fodder, as thatch, for many handicrafts, for biofuel, and as a source of expensive essential oil. Vetiver Grass is a safe and valuable addition to any smallholder farm or agroforestry system as well as for those working in infrastructure. Vetiver contains an oil that repels insects.</p>
+          
+        </div>
+      </div>
+      <div class="col-xs-12 col-md-6">
+        <div class="about-media">
+          <img src="img/vetiver.png" alt="Healthy and Stress-Free Tips">
+          
+          <!-- <div class="button-container" style="margin-top: 20px; text-align:center;">
+              <a href="tel:+18557616800" class="btn btn-custom btn-lg page-scroll" style="margin: 0 auto;">For More Tips</a>
+            </div> -->
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Side Effects Section -->
+<!-- <div id="services">
+  <div class="container">
+    <div class="col-md-10 col-md-offset-1 section-title text-center">
+      <h2>Side Effects of Anxiety Pills</h2>
+      <hr>
+      <p>While anxiety pills can be effective in managing symptoms, they are not without potential side effects. It's crucial to be aware of these side effects to make an informed decision about your treatment. Here are some common side effects associated with anxiety medications:</p>
+    </div>
+    <div class="row">
+      <div class="col-md-3 text-center">
+        <div class="service-media">
+          <img src="img/drowsiness.png" alt="Side Effect">
+        </div>
+        <div class="service-desc">
+          <h3>Drowsiness and Dizziness</h3>
+          <p>One of the most common side effects is drowsiness. Many users report feeling excessively tired or lethargic after taking the medication.Another frequent side effect is dizziness. Users may feel unsteady or lightheaded, especially when standing up quickly. <strong><a href="tel:+18557616800" style="color:white;">Know More >></a></strong></p>
+        </div>
+      </div>
+      <div class="col-md-3 text-center">
+        <div class="service-media">
+          <img src="img/respiratory.png" alt="Side Effect">
+        </div>
+        <div class="service-desc">
+          <h3>Respiratory Issues</h3>
+          <p>In rare cases, these medications can cause respiratory depression, leading to shallow or slow breathing. <strong><a href="tel:+18557616800" style="color:white;">Know More >></a></strong></p>
+        </div>
+      </div>
+      <div class="col-md-3 text-center">
+        <div class="service-media">
+          <img src="img/physical.png" alt="Side Effect">
+        </div>
+        <div class="service-desc">
+          <h3>Physical Symptoms</h3>
+          <p>Physical side effects can include nausea, dry mouth, and headaches. Some users may also experience blurred vision, slurred speech, or muscle weakness, which can affect daily functioning. <strong><a href="tel:+18557616800" style="color:white;">Know More >></a></strong></p>
+        </div>
+      </div>
+      <div class="col-md-3 text-center">
+        <div class="service-media">
+          <img src="img/doublevision.png" alt="Side Effect">
+        </div>
+        <div class="service-desc">
+          <h3>Double Vision</h3>
+          <p>One potential side effect of anxiety medication is double vision, also known as diplopia. This condition causes an individual to see two images of a single object, which can be disorienting and impair daily activities. <strong><a href="tel:+18557616800" style="color:white;">Know More >></a></strong></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div> -->
+
+<!-- Ban Information Section -->
+<!-- <div id="portfolio">
+  <div class="container">
+    <div class="section-title text-center center">
+      <h2>Why Anxiety Pills Are Banned in  USA</h2>
+      <hr>
+      <p>Certain anxiety medications have been banned or are strictly regulated in the United States due to their potential for abuse, dependency, and serious side effects. Here's why some anxiety pills are banned in  USA</p>
+    </div>
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="portfolio-item d-flex">
+          <div class="col-md-6 hover-bg">
+            <img src="img/bannedpills.webp" class="img-responsive" alt="Why They Are Banned">
+          </div>
+          <div class="col-md-6">
+            <div class="content">
+              <h4>Reasons for the Ban</h4>
+              <ul>
+                <li><strong>Risk of Abuse and Addiction:</strong> Some anxiety medications are classified as controlled substances because they have a high potential for abuse and addiction. Over time, users may develop a tolerance, requiring higher doses to achieve the same effect, leading to dependence and addiction. <a href="tel:+18557616800">Know More</a></li><br>
+                <li><strong>Severe Side Effects:</strong> The potential for severe side effects, such as respiratory depression, overdose, and even death, has led to the banning or strict regulation of certain anxiety pills. The risk increases when these medications are combined with other substances like alcohol or opioids. <a href="tel:+18557616800">know More</a></li><br>
+                <li><strong>Withdrawal Symptoms:</strong> Stopping these medications suddenly can lead to severe withdrawal symptoms, including rebound anxiety, seizures, and hallucinations. The difficulty in discontinuing use safely has contributed to the decision to ban these medications. <a href="tel:+18557616800">Know More</a></li><br>
+                <li><strong>Legal and Regulatory Concerns:</strong> The US government has implemented strict regulations to control the distribution and use of anxiety medications to protect public health. The banning of certain pills reflects the government's efforts to minimize the risk of abuse and ensure safe medical practices. <a href="tel:+18557616800">Know More</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div> -->
+
+
+<!-- Access Section -->
+
+
+
+<!-- <div id="testimonials" class="text-center">
+  <div class="overlay">
+    <div class="container">
+      <div class="section-title">
+        <h2>How to Access Anxiety Pills in  USA</h2>
+        <hr>
+      </div>
+      <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+          <div id="" style="color:white;">
+            <p style="font-size:18px;">If you're seeking these anxiety pills within the United States, it's important to approach the process with caution and ensure you're obtaining them through safe and legal means. While these medications are tightly regulated due to their potential for misuse and dependency, there are legitimate avenues to access them.</p>
+            <p style="font-size:18px;">To receive guidance on how to obtain these pills, it's crucial to consult with a healthcare professional who can assess your specific needs and provide a prescription if deemed necessary. However, if you're looking for more direct assistance or have questions about obtaining these medications, we're here to help.</p>
+            <p style="font-size:18px;">Our knowledgeable team is ready to provide you with the information you need and guide you through the appropriate steps. Whether you need advice on the legal aspects or simply want to explore your options, don't hesitate to reach out.</p>
+            <p style="font-size:18px;"><strong>Contact Us:</strong><br>
+              Call us at <strong><a href="tel:+18557616800" style="color:white;">+1 (855) 761 6800</a></strong> for personalized support and further details on how you can access these anxiety medications.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div> -->
+
+<!-- Contact Section -->
+<div id="contact" class="text-center">
+  <div class="container">
+    <div class="section-title text-center">
+      <h2>Contact Us for Support</h2>
+      <hr>
+      <p>Contact us today for a free consultation. Let's get started on your dream garden today!</p>
+    </div>
+    <div class="col-md-10 col-md-offset-1 contact-info">
+      <div class="col-md-4">
+        <h3>Address</h3>
+        <hr>
+        <div class="contact-item">
+          <p>123 Wellness Street,</p>
+          <p>Health City, CA 98765</p>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <h3>Working Hours</h3>
+        <hr>
+        <div class="contact-item">
+          <p>Monday-Saturday: 09:00 - 17:00</p>
+          <p>Sunday: CLOSED</p>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <h3>Contact Info</h3>
+        <hr>
+        <div class="contact-item">
+          <p>Phone: +1-800-555-1234</p>
+          <p>Email: support@anxietyhelp.com</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-8 col-md-offset-2">
+      <h3>Leave us a message</h3>
+      <form name="sentMessage" id="contactForm" novalidate>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <input type="text" id="name" class="form-control" placeholder="Name" required="required">
+              <p class="help-block text-danger"></p>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group">
+              <input type="email" id="email" class="form-control" placeholder="Email" required="required">
+              <p class="help-block text-danger"></p>
+            </div>
+          </div>
+        </div>
+        <div class="form-group">
+          <textarea name="message" id="message" class="form-control" rows="4" placeholder="Message" required></textarea>
+          <p class="help-block text-danger"></p>
+        </div>
+        <div id="success"></div>
+        <button type="submit" class="btn btn-custom btn-lg">Send Message</button>
+      </form>
+    </div>
+  </div>
+</div>
+
+<!-- Footer Section -->
+<div id="footer">
+  <div class="container text-center">
+    <div class="col-md-8 col-md-offset-2">
+      
+      <p>&copy; 2024 V Gardener</p>
+    </div>
+  </div>
+</div>
+<script type="text/javascript" src="js/jquery.1.11.1.js"></script> 
+<script type="text/javascript" src="js/bootstrap.js"></script> 
+<script type="text/javascript" src="js/SmoothScroll.js"></script> 
+<script type="text/javascript" src="js/nivo-lightbox.js"></script> 
+<script type="text/javascript" src="js/jquery.isotope.js"></script> 
+<script type="text/javascript" src="js/owl.carousel.js"></script> 
+<script type="text/javascript" src="js/jqBootstrapValidation.js"></script> 
+<script type="text/javascript" src="js/contact_me.js"></script> 
+<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
