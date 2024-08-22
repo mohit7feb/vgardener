f7e93e7d87d059d207679bba64ba39e03eac03aa
mohit7feb <45337317+mohit7feb@users.noreply.github.com>
2024-08-22 14:24:37 +0530

    Add files via upload

---
+++ ngrass.php
@@ -0,0 +1,382 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>N Grass</title>
+<meta name="description" content="">
+<meta name="author" content="">
+
+<!-- Favicons
+    ================================================== -->
+<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+
+<!-- Bootstrap -->
+<link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.css">
+
+<!-- Slider
+    ================================================== -->
+<link href="css/owl.carousel.css" rel="stylesheet" media="screen">
+<link href="css/owl.theme.css" rel="stylesheet" media="screen">
+
+<!-- Stylesheet
+    ================================================== -->
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/style2.css">
+<link rel="stylesheet" type="text/css" href="css/nivo-lightbox/nivo-lightbox.css">
+<link rel="stylesheet" type="text/css" href="css/nivo-lightbox/default.css">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+
+
+<style>
+    .portfolio-item.d-flex {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.portfolio-item .hover-bg {
+  flex-basis: 50%;
+}
+
+.portfolio-item .content {
+  flex-basis: 50%;
+  padding-left: 20px;
+}
+
+@media (max-width: 768px) {
+  .portfolio-item.d-flex {
+    flex-direction: column;
+  }
+
+  .portfolio-item .hover-bg,
+  .portfolio-item .content {
+    flex-basis: 100%;
+    padding-left: 0;
+  }
+
+  .portfolio-item .hover-bg img {
+    width: 100%;
+    height: auto;
+  }
+}
+
+</style>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+<!-- Navigation
+    ==========================================-->
+<nav id="menu" class="navbar navbar-default navbar-fixed-top">
+  <div class="container"> 
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+      <a class="navbar-brand page-scroll" href="#page-top">V Gardener</a> </div>
+    
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="index.php" class="page-scroll">About</a></li>
+        <li><a href="index.php" class="page-scroll">Services</a></li>
+        <li><a href="index.php" class="page-scroll">Gallery</a></li>
+        <li><a href="index.php" class="page-scroll">Testimonials</a></li>
+        <li><a href="vgrass.php" class="page-scroll">vgrass</a></li>
+        <li><a href="ngrass.php" class="page-scroll">Ngrass</a></li>
+        <li><a href="policy.php" class="page-scroll">Privacy Policy</a></li>
+        <li><a href="#contact" class="page-scroll">Contact</a></li>
+      </ul>
+    </div>
+    <!-- /.navbar-collapse --> 
+  </div>
+  <!-- /.container-fluid --> 
+</nav>
+<!-- Header -->
+<header id="header">
+  <div class="intro">
+    <div class="overlay">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2 intro-text">
+            <h1>Pain Management Awareness</h1>
+            <p>Understanding pain management and its impact on overall well-being.<br>
+              Explore strategies to manage pain effectively and live comfortably.</p>
+              <a href="tel:+18557616800" class="btn btn-custom btn-lg page-scroll" style="text-transform:none; border-radius:10px;">Know More</a>
+              </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+
+<!-- Pills Section -->
+<div id="about">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-md-6">
+        <div class="about-text">
+        <h2> Want <span>To Be Pain-Free?</span></h2>
+        <hr>
+          <p>Pain is a distressing feeling that can be sharp or dull, and can be felt in one area of the body or all over. It can be caused by intense or damaging stimuli, such as an injury, disease, or inflammation. Pain medicines can work in a variety of ways to relieve pain, including:</p>
+          <ul>
+            <li><strong>Changing pain messages in the brain:</strong> Opioid medicines change the way the brain processes pain messages, which can make them addictive. <a href="tel:+18557616800">Know More</a></li><br>
+            <li><strong>Turning down pain signals:</strong> Some medications reduce the volume of pain signals that the nervous system sends through the body. <a href="tel:+18557616800">Know More</a></li><br>
+            <!-- <li><strong>Changing brain chemistry:</strong> Low doses of antidepressants can help with chronic low back pain by changing the levels of certain chemicals in the brain, which can change how the brain notices pain. <a href="tel:+18557616800">Know More</a></li><br> -->
+            <!-- <li><strong>Take Breaks and Relax:</strong> It's essential to take time for yourself. Engage in hobbies, read, listen to music, or simply relax. Taking regular breaks from work and responsibilities helps refresh your mind. <a href="tel:+18557616800">Learn More</a></li><br>
+            <li><strong>Utilize Alternative Therapies:</strong> Complementary treatments like acupuncture or massage therapy may provide relief from chronic pain and enhance overall well-being. <a href="tel:+18557616800">Learn More</a></li><br>
+            <li><strong>Stay Hydrated:</strong> Drinking adequate water is crucial for maintaining joint lubrication and overall body function. Proper hydration can help prevent muscle cramps and reduce the risk of pain-related conditions. <a href="tel:+18557616800">Learn More</a></li> -->
+            <div class="button-container" style="margin-top: 20px; text-align:center;">
+              <a href="tel:+18557616800" class="btn btn-custom btn-lg page-scroll" style="margin: 0 auto;">Know More</a>
+            </div>
+          </ul>
+        </div>
+      </div>
+
+            
+
+      <div class="col-xs-12 col-md-6">
+        <div class="about-media">
+          <img src="img/vcdin.png" alt="Pain Management Tips">
+
+              
+            <!-- <div class="button-container" style="margin-top: 20px; text-align:center;">
+              <a href="tel:+18557616800" class="btn btn-custom btn-lg page-scroll" style="margin: 0 auto;">Know More</a>
+            </div> -->
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Tips Section -->
+<div id="portfolio">
+  <div class="container">
+    <div class="section-title text-center center">
+      <h2>Stay Healthy and Painless</h2>
+      <hr>
+      <p>Managing pain is essential for improving your quality of life. Here are some strategies to help you effectively manage pain and maintain well-being:</p>
+    </div>
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="portfolio-item d-flex">
+          <div class="col-md-6 hover-bg">
+            <img src="img/tipsforhealth2.jpg" class="img-responsive" alt="Reasons for Restriction">
+          </div>
+          <div class="col-md-6">
+            <div class="content">
+              <!-- <h4>Reasons for Restrictions</h4> -->
+              <ul>
+                <li><strong>Connect with Others:</strong> Social connections are vital for emotional well-being. Spend time with loved ones, talk about your feelings, and don't hesitate to seek support when needed. <a href="tel:+18557616800">Learn More</a></li><br>
+                <li><strong>Engage in Physical Therapy:</strong> Physical therapy can help alleviate pain and improve mobility. Exercises tailored to your condition can strengthen muscles and reduce discomfort. <a href="tel:+18557616800">Learn More</a></li><br>
+                <li><strong>Adopt a Healthy Lifestyle:</strong> A balanced diet and regular exercise contribute to overall health and can help manage chronic pain. <a href="tel:+18557616800">Learn More</a></li><br>
+                <li><strong>Take Breaks and Relax:</strong> It's essential to take time for yourself. Engage in hobbies, read, listen to music, or simply relax. Taking regular breaks from work and responsibilities helps refresh your mind. <a href="tel:+18557616800">Learn More</a></li><br>
+                <!-- <li><strong>Utilize Alternative Therapies:</strong> Complementary treatments like acupuncture or massage therapy may provide relief from chronic pain and enhance overall well-being. <a href="tel:+18557616800">Learn More</a></li><br> -->
+                <!-- <li><strong>Stay Hydrated:</strong> Drinking adequate water is crucial for maintaining joint lubrication and overall body function. Proper hydration can help prevent muscle cramps and reduce the risk of pain-related conditions. <a href="tel:+18557616800">Learn More</a></li> -->
+                <div class="button-container" style="margin-top: 20px; text-align:center;">
+                  <a href="tel:+18557616800" class="btn btn-custom btn-lg page-scroll" style="margin: 0 auto;">For More Tips</a>
+                </div>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Side Effects Section -->
+<div id="services">
+  <div class="container">
+    <div class="col-md-10 col-md-offset-1 section-title text-center">
+      <h2>Side Effects of Painkillers</h2>
+      <hr>
+      <p>Painkillers can be effective in managing pain but come with potential side effects. Understanding these side effects helps in making informed decisions about pain management. Here are some common side effects associated with pain medications:</p>
+    </div>
+    <div class="row">
+      <div class="col-md-3 text-center">
+        <div class="service-media">
+          <img src="img/pain_side_1.png" alt="Side Effect">
+        </div>
+        <div class="service-desc">
+          <h3>Drowsiness and hallucination</h3>
+          <p>Drowsiness and fatigue are common side effects. Users may experience significant tiredness or lethargy, which can affect daily activities. <strong><a href="tel:+18557616800" style="color:white;">Know More >></a></strong></p>
+        </div>
+      </div>
+      <div class="col-md-3 text-center">
+        <div class="service-media">
+          <img src="img/respiratory.png" alt="Side Effect">
+        </div>
+        <div class="service-desc">
+          <h3>Respiratory Depression</h3>
+          <p>In severe cases, painkillers may lead to respiratory depression, causing shallow or slowed breathing, which requires careful monitoring. <strong><a href="tel:+18557616800" style="color:white;">Know More >></a></strong></p>
+        </div>
+      </div>
+      <div class="col-md-3 text-center">
+        <div class="service-media">
+          <img src="img/pain_side_3.png" alt="Side Effect">
+        </div>
+        <div class="service-desc">
+          <h3>Nausea and Digestive Issues</h3>
+          <p>Nausea, vomiting, stomach pain and constipation are possible side effects. Users may also experience a loss of appetite or digestive discomfort. <strong><a href="tel:+18557616800" style="color:white;">Know More >></a></strong></p>
+        </div>
+      </div>
+      <div class="col-md-3 text-center">
+        <div class="service-media">
+          <img src="img/pain_side_4.png" alt="Side Effect">
+        </div>
+        <div class="service-desc">
+          <h3>Allergic Skin Reaction on Body</h3>
+          <p>Some pain medications can cause rare but serious skin reactions, including skin reddening, blisters, and separation of the skin's upper layers. <strong><a href="tel:+18557616800" style="color:white;">Know More >></a></strong></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Ban Information Section -->
+<div id="portfolio">
+  <div class="container">
+    <div class="section-title text-center center">
+      <h2>Why Certain Painkillers Are Banned in USA</h2>
+      <hr>
+      <p>Some pain medications are restricted or regulated due to their potential for abuse, dependency, and severe side effects. Here’s why some painkillers face such restrictions:</p>
+    </div>
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="portfolio-item d-flex">
+          <div class="col-md-6 hover-bg">
+            <img src="img/bannedpain.jpg" class="img-responsive" alt="Reasons for Restriction">
+          </div>
+          <div class="col-md-6">
+            <div class="content">
+              <h4>Reasons for Restrictions</h4>
+              <ul>
+              <li><strong>Potential for Overdose:</strong> Certain painkillers, especially those with high potency, pose a significant risk of overdose. <a href="tel:+18557616800">Know More</a></li><br>
+                <li><strong>Interactions with Other Medications:</strong> Painkillers can interact adversely with other medications, leading to dangerous side effects. <a href="tel:+18557616800">Know More</a></li><br>
+                <li><strong>Impact on Mental Health:</strong> Some painkillers can affect mood and mental health, potentially leading to depression. <a href="tel:+18557616800">Know More</a></li><br>
+                <li><strong>Potential for Illicit Use:</strong> Certain painkillers are commonly diverted for illicit use, contributing to the risk of public health crises. <a href="tel:+18557616800">Know More</a></li><br>
+                <li><strong>Lack of Long-Term Safety Data:</strong> For some painkillers, there may be insufficient data on long-term safety and efficacy. <a href="tel:+18557616800">Know More</a></li><br>
+                <li><strong>High Risk of Habit Formation:</strong> Some painkillers have a high risk of habit formation, resulting in users becoming reliant. <a href="tel:+18557616800">Know More</a></li>
+              
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Access Section -->
+<div id="testimonials" class="text-center">
+  <div class="overlay">
+    <div class="container">
+      <div class="section-title">
+        <h2>How to Access PainKillers Safely in USA</h2>
+        <hr>
+      </div>
+      <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+          <div id="" style="color:white;">
+            <p style="font-size:18px;">If you need to access pain medications, it’s important to do so through safe and legal channels. Given the potential for misuse and dependency, these medications are tightly regulated.</p>
+            <p style="font-size:18px;">Consult with a healthcare professional to assess your needs and obtain a prescription if necessary. If you have questions about accessing these medications, our team is here to assist you.</p>
+            <p style="font-size:18px;">For personalized support and information on how to obtain pain medications legally and safely, contact us.</p>
+            <p style="font-size:18px;"><strong>Contact Us:</strong><br>
+              Call us at <strong><a href="tel:+18557616800" style="color:white;">+1 (855) 761 6800</a></strong> for detailed guidance and support regarding pain management options.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Contact Section -->
+<!-- <div id="contact" class="text-center">
+  <div class="container">
+    <div class="section-title text-center">
+      <h2>Contact Us for Support</h2>
+      <hr>
+      <p>If you're struggling with anxiety and need support, don't hesitate to reach out. We're here to help.</p>
+    </div>
+    <div class="col-md-10 col-md-offset-1 contact-info">
+      <div class="col-md-4">
+        <h3>Address</h3>
+        <hr>
+        <div class="contact-item">
+          <p>123 Wellness Street,</p>
+          <p>Health City, CA 98765</p>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <h3>Working Hours</h3>
+        <hr>
+        <div class="contact-item">
+          <p>Monday-Saturday: 09:00 - 17:00</p>
+          <p>Sunday: CLOSED</p>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <h3>Contact Info</h3>
+        <hr>
+        <div class="contact-item">
+          <p>Phone: +1-800-555-1234</p>
+          <p>Email: support@anxietyhelp.com</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-8 col-md-offset-2">
+      <h3>Leave us a message</h3>
+      <form name="sentMessage" id="contactForm" novalidate>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <input type="text" id="name" class="form-control" placeholder="Name" required="required">
+              <p class="help-block text-danger"></p>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group">
+              <input type="email" id="email" class="form-control" placeholder="Email" required="required">
+              <p class="help-block text-danger"></p>
+            </div>
+          </div>
+        </div>
+        <div class="form-group">
+          <textarea name="message" id="message" class="form-control" rows="4" placeholder="Message" required></textarea>
+          <p class="help-block text-danger"></p>
+        </div>
+        <div id="success"></div>
+        <button type="submit" class="btn btn-custom btn-lg">Send Message</button>
+      </form>
+    </div>
+  </div>
+</div> -->
+
+<!-- Footer Section -->
+<div id="footer">
+  <div class="container text-center">
+    <div class="col-md-8 col-md-offset-2">
+      
+      <p>&copy; 2024 V Gardener</p>
+    </div>
+  </div>
+</div>
+<script type="text/javascript" src="js/jquery.1.11.1.js"></script> 
+<script type="text/javascript" src="js/bootstrap.js"></script> 
+<script type="text/javascript" src="js/SmoothScroll.js"></script> 
+<script type="text/javascript" src="js/nivo-lightbox.js"></script> 
+<script type="text/javascript" src="js/jquery.isotope.js"></script> 
+<script type="text/javascript" src="js/owl.carousel.js"></script> 
+<script type="text/javascript" src="js/jqBootstrapValidation.js"></script> 
+<script type="text/javascript" src="js/contact_me.js"></script> 
+<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
